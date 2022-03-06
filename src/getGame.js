42c7985ac5eb0277009a5a45c89c7d87b9d9c430
Luis Gonzales <luis.gonzales35@unmsm.edu.pe>
2022-03-06 16:50:02 -0500

    Buscar un game

---
+++ src/getGame.js
@@ -0,0 +1,25 @@
+const AWS = require("aws-sdk");
+
+const getGame = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const { id } = event.pathParameters;
+
+  const result = await dynamodb
+    .get({
+      TableName: "Games",
+      Key: { id },
+    })
+    .promise();
+
+  const task = result.Item;
+
+  return {
+    status: 200,
+    body: task,
+  };
+};
+
+module.exports = {
+  getGame,
+};
